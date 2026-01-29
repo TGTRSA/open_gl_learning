9f0d08fe54a9cb89d51b8ed7a70169a5d995dae9
TGTRSA <tsukimi.ukiyo@gmail.com>
2026-01-29 20:13:12 +0000

    [Added] Struct for window params and create function to create the window based on paramaters

--- window.cpp
+++ window.cpp
@@ -3,6 +3,17 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+struct gl_window {
+    int height;
+    int width;
+    const char title[12];
+
+    GLFWwindow* create(int width, int height){
+        GLFWwindow* window = glfwCreateWindow( width, height,title, NULL , NULL);
+        return window;
+    }
+
+};
 
 int main() {
 
@@ -12,6 +23,22 @@ int main() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    // Initialising glad
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
 
     return 0;
 }
