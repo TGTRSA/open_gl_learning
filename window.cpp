c2a53452002e11bbeaedcec5f7a935a1b850d826
TGTRSA <tsukimi.ukiyo@gmail.com>
2026-02-07 11:42:50 +0000

    [Change] Code for fragment and vertex shaders

--- window.cpp
+++ window.cpp
@@ -2,21 +2,35 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <iostream>
-
 const char *vertexShaderSource = "#version 330 core\n"
-"layout (location = 0) in vec3 aPos;\n"
+"layout (location = 0) in vec3 aPos;\n // position has attribute position 0"
+"layout (location = 1) in vec3 aColor;\n // color has attribute position 1"
+"out vec3 ourColor;\n // output a color to the fragment shader"
 "void main()\n"
 "{\n"
-" gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"gl_Position = vec4(aPos, 1.0);\n"
+"ourColor = aColor;\n // set ourColor to input color from the vertex data"
 "}\0";
-
-
+// const char *vertexShaderSource = "#version 330 core\n"
+// "layout (location = 0) in vec3 aPos;\n"
+// "void main()\n"
+// "{\n"
+// " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+// "}\0";
 const char *fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
+"in vec3 ourColor;\n"
 "void main()\n"
 "{\n"
-"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+"FragColor = vec4(ourColor, 1.0);\n"
 "}\0";
+
+// const char *fragmentShaderSource = "#version 330 core\n"
+// "out vec4 FragColor;\n"
+// "void main()\n"
+// "{\n"
+// "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+// "}\0";
 
 struct gl_window {
     int height;
